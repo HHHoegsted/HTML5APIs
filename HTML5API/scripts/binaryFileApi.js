e53f8f443eda2fe0c370ddcef608d767a5ebb6bd
hhhoegsted <hh.hoegsted@gmail.com>
2019-09-04 13:03:24 +0200

    Binary file handling

---
+++ HTML5API/scripts/binaryFileApi.js
@@ -0,0 +1,16 @@
+﻿function loadBinaryFile() {
+    const fileElement = document.querySelector('#binaryFile');
+
+    if (fileElement.files.length !== 0 && fileElement.files[0].type.match(/image.*/)) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            document.querySelector('#imageHolder').src = e.target.result;
+        };
+        reader.onerror = (e) => {
+            document.queryselector('#errorMsg').innerText = 'Something went wrong loading the file: '+ e;
+        };
+        reader.readAsDataURL(fileElement.files[0]);
+    } else {
+        document.querySelector('#errorMsg').innerText = 'Only image files please';
+    }
+}
