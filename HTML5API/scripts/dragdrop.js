105abccca9e8851fcd6dc1c00a7f30d8725525fa
hhhoegsted <hh.hoegsted@gmail.com>
2019-09-04 10:24:14 +0200

    Drag and Drop implemented

---
+++ HTML5API/scripts/dragdrop.js
@@ -0,0 +1,15 @@
+﻿function allowDrop(event) {
+    event.preventDefault();
+
+}
+
+function drag(event) {
+    event.dataTransfer.setData('img', event.target.id);
+}
+
+function drop(event) {
+    event.preventDefault();
+    let data = event.dataTransfer.getData('img');
+    event.target.appendChild(document.getElementById(data));
+    document.getElementById('after').innerHTML = "Something got dropped into the box.";
+}
