0dc7425f97cf0f996b8cd41149551b7482a58b0f
hhhoegsted <hh.hoegsted@gmail.com>
2019-09-04 14:09:50 +0200

    localstorage og sessionstorage

---
+++ HTML5API/scripts/storage.js
@@ -0,0 +1,9 @@
+﻿sessionStorage.setItem("name", "Mark");
+localStorage.setItem("name", "Maria");
+
+//alternative
+sessionStorage.UserName = "Trump";
+sessionStorage.Country = "Russia";
+
+localStorage.UserName = "Xi";
+localStorage.Country = "China";
---
+++ HTML5API/scripts/storagetest.js
@@ -0,0 +1,15 @@
+﻿document.writeln('--> Session Storage <--<br>');
+document.writeln(sessionStorage.name);
+document.writeln('<br>');
+document.writeln(sessionStorage.UserName);
+document.writeln('<br>');
+document.writeln(sessionStorage.Country);
+document.writeln('<br>'); document.writeln('<br>');
+
+document.writeln('--> Local Storage <--<br>');
+document.writeln(localStorage.name);
+document.writeln('<br>');
+document.writeln(localStorage.UserName);
+document.writeln('<br>');
+document.writeln(localStorage.Country);
+document.writeln('<br>');
