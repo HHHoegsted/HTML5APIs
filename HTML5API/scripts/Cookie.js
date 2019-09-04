16e44e25701183bfec121317d1192469104c5fb4
hhhoegsted <hh.hoegsted@gmail.com>
2019-09-04 13:44:21 +0200

    Cookies!

---
+++ HTML5API/scripts/Cookie.js
@@ -0,0 +1,16 @@
+﻿window.onload = () => {
+    if (document.cookie.length !== 0) {
+        const getCookieArray = document.cookie.split('=');
+        document.bgColor = getCookieArray[1];
+        document.querySelector('#selectID').value = getCookieArray[1];
+    }
+};
+
+let setCookie = () => {
+    const choice = document.querySelector('#selectID').value;
+
+    if (choice !== 'VælgFarve') {
+        document.bgColor = choice;
+        document.cookie = "color=" + choice + "; expires=31 December 2020 00:01:00 UTC";
+    }
+}
